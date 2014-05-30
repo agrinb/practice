1bef38999f019443519d2d6e991a5219bcf59a95
Alek <agrinb@gmail.com>
2014-05-29 20:37:05 -0400

    added new movie form, refactored movie_details and movie_actors methods

--- sqlpractice.sql
+++ sqlpractice.sql
@@ -19,5 +19,45 @@ CREATE TABLE ingredients (
 
 
 
-INSERT INTO tasks (description, estimated_duration, created_at)
-  VALUES ('Learn SQL', 180, now());
+INSERT INTO recipes (name, time, directions)
+  VALUES ('Green Eggs & Ham', 25, '1. Cook the eggs.
+2. Cook the ham.
+3. Combine.
+';
+
+INSERT INTO recipes (name, time, directions)
+  VALUES ('Fried Green Tomatoes', '', '1. Slice the tomatoes 1/2 inch thick.
+2. Whisk eggs and milk together.
+3. Dip tomatoes in egg mixture and then bread crumbs.
+4. Heat oil in a large skillet.
+5. Fry the tomatoes in the oil.
+';
+
+
+INSERT INTO recipes (name, time, directions)
+  VALUES ('Martini', '','1. Pour all ingredients into mixing glass with ice cubes.
+2. Stir well.
+3. Strain in chilled martini cocktail glass.
+4. Squeeze oil from lemon peel onto the drink, or garnish with olive.';
+
+INSERT INTO ingredients (quantity, food, yields, recipe_id)
+  VALUES
+(4,'Green Eggs & Ham', 2, 1)
+(.5, Ham)
+(3 large green tomatoes
+2 eggs
+1/2 cup milk
+1/2 cup breadcrumbs
+1 quart vegetable oil
+
+
+
+
+Ingredients:
+4 green eggs
+1/2 lb ham
+
+
+
+
+
